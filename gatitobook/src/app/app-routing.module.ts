1aeb8fdddd1d34364ec47aa1d76096800e2fa3bd
Rafael Aranda Martins <skema1114@hotmail.com>
2021-05-26 17:37:57 -0300

    Terminando a parte 01

---
+++ gatitobook/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'home'},
+  // AQUI EM BAIXO É UMA FUNÇÃO DE ROTA UTILIZANDO O LAZY LOAD
+  {path: 'home', loadChildren: () => import('./home/home.module').then((modulo) => modulo.HomeModule)},
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
